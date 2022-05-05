99ac8cde36166e1a901f8031d9e648470cdb3253
Victor11019 <kaa5558@gmail.com>
2022-05-06 01:11:23 +0800

    project

---
+++ tebot.py
@@ -0,0 +1,11 @@
+import telebot;
+bot =  telebot.TeleBot('5206462331:AAFvBFyXRcyASFu5jt4g8cExwrC4FeZUptw');
+@bot.message_handler(content_types=['text'])
+def get_text_message(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+bot.polling(none_stop=True, interval=0)
